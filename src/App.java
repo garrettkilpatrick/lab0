721963a707882786e2db7dd0bd0add3e97f0aaa5
garrettkilpatrick <garrettkilpatrick30@gmail.com>
2022-08-31 11:27:15 -0500

    First snapshot

---
+++ src/App.java
@@ -0,0 +1,16 @@
+//package myApp;
+
+import cs3331.lab0.helper.HelloWorldFrame;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    public void start(Stage stage){
+        new HelloWorldFrame(stage);
+    }
+
+    public static void main(String[] args){
+        System.out.println("Hello");
+        launch(args);
+    }
+}
